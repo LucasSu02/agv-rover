57ce17ac20e1e1aa57e35d41c97c30b83e1ef6e6
Fawzan <fawzanalim@gmail.com>
2024-10-21 17:04:19 +0800

    Add navigation

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -37,9 +37,18 @@ def generate_launch_description():
                                    '-entity', 'rover'],
                         output='screen')
 
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time': True}],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
+    
     # Launch them all!
     return LaunchDescription([
         rsp,
+        twist_mux,
         gazebo,
         spawn_entity,
     ])
